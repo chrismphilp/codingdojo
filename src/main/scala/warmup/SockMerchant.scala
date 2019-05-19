d01965cc9501f4c781c2ecb532bbccbee3e1f39d
chrismphilp <chrismphilp@hotmail.co.uk>
2019-05-19 18:41:20 +0100

    Setting up project

---
+++ src/main/scala/warmup/SockMerchant.scala
@@ -0,0 +1,18 @@
+package warmup
+
+import scala.collection.mutable
+
+object SockMerchant {
+  def sockMerchant(n: Int, ar: Array[Int]): Int = {
+    val sockStore: mutable.HashMap[Int, Int] = new mutable.HashMap[Int, Int]()
+    var pairCount = 0
+    ar.foreach((value: Int) => {
+      if (sockStore.contains(value)) {
+        pairCount+=1
+        sockStore.remove(value)
+      }
+      else sockStore.put(value, 1)
+    })
+    pairCount
+  }
+}
