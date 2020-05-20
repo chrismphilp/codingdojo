84917037975bf66d8f2f431398e40f076afbbd72
Christopher Philp <chrismphilp@hotmail.co.uk>
2020-05-20 16:42:03 +0100

    Working ZigZagConversionTest.cpp, but needs speed improvement

--- code/cpp/src/leetcode/zig-zag-conversion/ZigZagConversion.cpp
+++ code/cpp/src/leetcode/zig-zag-conversion/ZigZagConversion.cpp
@@ -1,10 +1,30 @@
+#include <unordered_map>
 #include "ZigZagConversion.h"
 
 std::string ZigZagConversion::convert(std::string s, int numRows) {
-    if (s.length() <= numRows || numRows <= 1) return s;
+    int stringLength = s.length();
+    if (stringLength <= numRows || numRows <= 1) return s;
 
-    for (unsigned int i = 0; i < numRows; i++) {
-        
+    std::unordered_map<int, std::string> mapOfRows;
+    bool down = false;
+    int currentRow = 0;
+
+    for (int i = 0; i < stringLength; i++) {
+        if (currentRow == (numRows - 1) || currentRow == 0) down = !down;
+
+        if (mapOfRows.find(currentRow) != mapOfRows.end()) {
+            std::string curr = mapOfRows.at(currentRow);
+            curr += s.at(i);
+            mapOfRows[currentRow] = curr;
+        } else {
+            mapOfRows[currentRow] = s.at(i);
+        }
+        down ? currentRow++ : currentRow--;
     }
-    return std::string();
+
+    std::string result = std::string();
+    for (int i = 0; i < numRows; i++) {
+        result.append(mapOfRows.at(i));
+    }
+    return result;
 }
--- code/cpp/test/leetcode/zig-zag-conversion/ZigZagConversionTest.cpp
+++ code/cpp/test/leetcode/zig-zag-conversion/ZigZagConversionTest.cpp
@@ -1,15 +1,21 @@
 #include "ZigZagConversionTest.h"
 
-TEST_F(ZigZagConversionTest, ShouldReturnTheSameStringForASingleRow) {
+TEST_F(ZigZagConversionTest, ShouldReturnTheSameStringForLessThanTwoRows) {
     std::string word = "ZIGZAG";
     ASSERT_EQ(zigZagConversion.convert(word, 0), word);
     ASSERT_EQ(zigZagConversion.convert(word, 1), word);
 }
 
 TEST_F(ZigZagConversionTest, ShouldReturnTheCorrectStringForTwoRows) {
-    ASSERT_EQ(zigZagConversion.convert("ABCDEF", 2), "");
+    ASSERT_EQ(zigZagConversion.convert("ABC", 2), "ACB");
+    ASSERT_EQ(zigZagConversion.convert("ABCDEF", 2), "ACEBDF");
 }
 
 TEST_F(ZigZagConversionTest, ShouldReturnTheCorrectStringForThreeRows) {
-//    ASSERT_EQ(zigZagConversion.convert("ABCDEF", 3), "AEBDFC");
+    ASSERT_EQ(zigZagConversion.convert("ABCD", 3), "ABDC");
+    ASSERT_EQ(zigZagConversion.convert("ABCDEF", 3), "AEBDFC");
+}
+
+TEST_F(ZigZagConversionTest, ShouldReturnTheCorrectStringForFourRows) {
+    ASSERT_EQ(zigZagConversion.convert("ABCDEF", 4), "ABFCED");
 }
