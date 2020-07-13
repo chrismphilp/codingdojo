8799cd5653a7ca080e953fab02cdec593cc65c7b
Christopher Philp <chrismphilp@hotmail.co.uk>
2020-07-13 18:17:35 +0100

    [master] | Improved solution for RomanToInteger

--- code/scala/src/main/java/leet_code/easy/RomanToInteger.java
+++ code/scala/src/main/java/leet_code/easy/RomanToInteger.java
@@ -3,36 +3,18 @@ package leet_code.easy;
 class RomanToInteger {
     public int romanToInt(String s) {
         int sum = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (i + 1 != s.length()) {
-                char a = s.charAt(i);
-                char b = s.charAt(i + 1);
-                int aValue = convertSingleRomanNumeral(a);
-                int bValue = convertSingleRomanNumeral(b);
-
-                if (aValue < bValue) {
-                    sum += (bValue - aValue);
-                    i++;
-                } else {
-                    sum += aValue;
-                }
-            } else {
-                sum += convertSingleRomanNumeral(s.charAt(i));
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            switch (c) {
+                case 'I': sum += (sum < 5) ? 1 : -1; break;
+                case 'V': sum += (sum < 10) ? 5 : -5; break;
+                case 'X': sum += (sum < 50) ? 10 : -10; break;
+                case 'L': sum += (sum < 100) ? 50 : -50; break;
+                case 'C': sum += (sum < 500) ? 100 : -100; break;
+                case 'D': sum += (sum < 1000) ? 500 : -500; break;
+                default: sum += (sum < 10000) ? 1000 : -1000;
             }
         }
         return sum;
-    }
-
-    private int convertSingleRomanNumeral(char c) {
-        switch (c) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            default: return 1000;
-        }
     }
 }
