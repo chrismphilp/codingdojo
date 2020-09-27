a0dbc2a78322e41ed770cd7be8dfb0838fb94917
Christopher Philp <chrismphilp@hotmail.co.uk>
2020-09-27 16:46:15 +0100

    [master] | Adding solution for GenerateParantheses

--- code/java/main/leet_code/medium/GenerateParentheses.java
+++ code/java/main/leet_code/medium/GenerateParentheses.java
@@ -1,12 +1,33 @@
 package leet_code.medium;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-// TODO
 public class GenerateParentheses {
-    public List<String> generateParenthesis(int n) {
 
-        return new ArrayList<>();
+    List<String> list = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        if (n == 0) return Collections.singletonList("");
+        createParenthesesString("(", 1, 1, 0, n);
+        return list;
+    }
+
+    private void createParenthesesString(String curr, int openBracketsUsed, int openBrackets, int closedBracketsUsed,
+                                         int n) {
+        if (closedBracketsUsed == n) {
+            list.add(curr);
+        } else if (openBracketsUsed == n) {
+            createParenthesesString(curr.concat(")"), openBracketsUsed, openBrackets - 1,
+                    closedBracketsUsed + 1, n);
+        } else {
+            if (openBracketsUsed < n) {
+                createParenthesesString(curr.concat("("), openBracketsUsed + 1,
+                        openBrackets + 1, closedBracketsUsed, n);
+            }
+            if (closedBracketsUsed < n && openBrackets > 0) {
+                createParenthesesString(curr.concat(")"), openBracketsUsed, openBrackets - 1,
+                        closedBracketsUsed + 1, n);
+            }
+        }
     }
 }
--- code/java/test/leet_code/medium/GenerateParenthesesTest.java
+++ code/java/test/leet_code/medium/GenerateParenthesesTest.java
@@ -3,8 +3,9 @@ package leet_code.medium;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class GenerateParenthesesTest {
@@ -18,30 +19,34 @@ public class GenerateParenthesesTest {
 
     @Test
     public void zero_input_should_return_single_element_empty_string_list() {
-        assertTrue(generateParentheses.generateParenthesis(1).contains(""));
+        List<String> list = generateParentheses.generateParenthesis(0);
+        assertTrue(list.contains(""));
+        assertEquals(1, list.size());
     }
 
     @Test
     public void one_input_should_return_single_element_matching_parentheses() {
-        assertTrue(generateParentheses.generateParenthesis(1).contains("()"));
+        List<String> list = generateParentheses.generateParenthesis(1);
+        assertTrue(list.contains("()"));
+        assertEquals(1, list.size());
     }
 
     @Test
     public void two_input_should_return_single_element_matching_parentheses() {
-        assertTrue(generateParentheses.generateParenthesis(2).containsAll(Arrays.asList(
-                "()()",
-                "(())"
-        )));
+        List<String> list = generateParentheses.generateParenthesis(2);
+        assertTrue(list.contains("()()"));
+        assertTrue(list.contains("(())"));
+        assertEquals(2, list.size());
     }
 
     @Test
     public void three_input_should_return_single_element_matching_parentheses() {
-        assertTrue(generateParentheses.generateParenthesis(3).containsAll(Arrays.asList(
-                "()()()",
-                "(())()",
-                "()(())",
-                "(()())",
-                "((()))"
-        )));
+        List<String> list = generateParentheses.generateParenthesis(3);
+        assertTrue(list.contains("()()()"));
+        assertTrue(list.contains("(())()"));
+        assertTrue(list.contains("()(())"));
+        assertTrue(list.contains("(()())"));
+        assertTrue(list.contains("((()))"));
+        assertEquals(5, list.size());
     }
 }
