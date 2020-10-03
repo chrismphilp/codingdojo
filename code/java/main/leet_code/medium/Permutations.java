44247a602cab51a73648738dcf935d8e6baa3ec7
Christopher Philp <chrismphilp@hotmail.co.uk>
2020-10-03 17:17:09 +0100

    [master] | Adding more efficient stolen solution

--- code/java/main/leet_code/medium/Permutations.java
+++ code/java/main/leet_code/medium/Permutations.java
@@ -1,41 +1,29 @@
 package leet_code.medium;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Permutations {
 
-    List<List<Integer>> store = new ArrayList<>();
-
     public List<List<Integer>> permute(int[] nums) {
-        if (nums.length == 0) return Collections.emptyList();
-        if (nums.length == 1) return Collections.singletonList(Collections.singletonList(nums[0]));
-
-        List<Integer> uniqueDigits = Arrays.stream(nums).boxed().collect(Collectors.toList());
-
-        for (int i = 0; i < nums.length; i++) {
-            List<Integer> tmp = new ArrayList<>(uniqueDigits);
-            tmp.remove(i);
-            createPermutations(Collections.singletonList(nums[i]), tmp);
-        }
-        return store;
+        List<List<Integer>> permutations = new ArrayList<>();
+        if (nums.length == 0) return permutations;
+        createPermutations(nums, 0, new ArrayList<>(), permutations);
+        return permutations;
     }
 
-    private void createPermutations(List<Integer> curr, List<Integer> remaining) {
-        if (remaining.size() == 0) {
-            store.add(curr);
+    private void createPermutations(int[] nums, int curr, List<Integer> remaining,
+                                    List<List<Integer>> permutations) {
+        if (remaining.size() == nums.length) {
+            permutations.add(remaining);
             return;
         }
 
-        for (int i = 0; i < remaining.size(); i++) {
-            List<Integer> tmpCurr = new ArrayList<>(curr);
-            List<Integer> tmpRemaining = new ArrayList<>(remaining);
-            tmpRemaining.remove(i);
-            tmpCurr.add(remaining.get(i));
-            createPermutations(tmpCurr, tmpRemaining);
+        for (int i = 0; i <= remaining.size(); i++) {
+            List<Integer> tmp = new ArrayList<>(remaining);
+            tmp.add(i, nums[curr]);
+            createPermutations(nums, curr + 1, tmp, permutations);
         }
     }
 }
