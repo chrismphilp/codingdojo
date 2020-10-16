6dcc52cd73c04460dfb573f0ea585c05a1ea0c07
Christopher Philp <chrismphilp@hotmail.co.uk>
2020-10-16 16:46:14 +0100

    [master] | Quicker solution using dynamic programming

--- code/java/main/leet_code/medium/UniquePaths.java
+++ code/java/main/leet_code/medium/UniquePaths.java
@@ -3,16 +3,15 @@ package leet_code.medium;
 // TODO: Improve speeds
 public class UniquePaths {
     public int uniquePaths(int m, int n) {
-        return remainingSteps(m, n, 1, 1);
-    }
+        int[][] gridMemo = new int[m][n];
+        for (int i = 0; i < m; i++) gridMemo[i][0] = 1;
+        for (int i = 0; i < n; i++) gridMemo[0][i] = 1;
 
-    private int remainingSteps(int gridX, int gridY, int currX, int currY) {
-        if (currX == gridX && currY == gridY) return 1;
-        else if (currX == gridX) return remainingSteps(gridX, gridY, currX, currY + 1);
-        else if (currY == gridY) return remainingSteps(gridX, gridY, currX + 1, currY);
-        else {
-            return remainingSteps(gridX, gridY, currX + 1, currY) +
-                    remainingSteps(gridX, gridY, currX, currY + 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                gridMemo[i][j] = gridMemo[i - 1][j] + gridMemo[i][j - 1];
+            }
         }
+        return gridMemo[m - 1][n - 1];
     }
 }
