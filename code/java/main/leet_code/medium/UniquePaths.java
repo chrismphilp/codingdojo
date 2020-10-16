9b0585d60bf637dbf51153701e0cb6cb6fc6739c
Christopher Philp <chrismphilp@hotmail.co.uk>
2020-10-16 16:39:09 +0100

    [master] | Initial solution for the UniquePaths problem

--- code/java/main/leet_code/medium/UniquePaths.java
+++ code/java/main/leet_code/medium/UniquePaths.java
@@ -1,8 +1,18 @@
 package leet_code.medium;
 
-// TODO
+// TODO: Improve speeds
 public class UniquePaths {
     public int uniquePaths(int m, int n) {
-        return 0;
+        return remainingSteps(m, n, 1, 1);
+    }
+
+    private int remainingSteps(int gridX, int gridY, int currX, int currY) {
+        if (currX == gridX && currY == gridY) return 1;
+        else if (currX == gridX) return remainingSteps(gridX, gridY, currX, currY + 1);
+        else if (currY == gridY) return remainingSteps(gridX, gridY, currX + 1, currY);
+        else {
+            return remainingSteps(gridX, gridY, currX + 1, currY) +
+                    remainingSteps(gridX, gridY, currX, currY + 1);
+        }
     }
 }
--- code/java/test/leet_code/medium/UniquePathsTest.java
+++ code/java/test/leet_code/medium/UniquePathsTest.java
@@ -15,7 +15,17 @@ public class UniquePathsTest {
     }
 
     @Test
-    public void should_return_correct_result_for_single_element_array() {
-        assertEquals(0, uniquePaths.uniquePaths(1, 2));
+    public void should_return_one_for_one_by_any_grid() {
+        assertEquals(1, uniquePaths.uniquePaths(1, 1));
+        assertEquals(1, uniquePaths.uniquePaths(5, 1));
+        assertEquals(1, uniquePaths.uniquePaths(1, 5));
+    }
+
+    @Test
+    public void should_return_correct_result_for_grids() {
+        assertEquals(28, uniquePaths.uniquePaths(3, 7));
+        assertEquals(70, uniquePaths.uniquePaths(5, 5));
+        assertEquals(8, uniquePaths.uniquePaths(8, 2));
+        assertEquals(48620, uniquePaths.uniquePaths(10, 10));
     }
 }
