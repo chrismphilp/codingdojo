0889e78879bac1c6893d3313a9d8aedf54b9616c
Christopher Philp <chrismphilp@hotmail.co.uk>
2021-04-03 18:04:33 +0100

    [master] | Improving MovingBalls solution to O(n)

--- code/java/main/leet_code/medium/MinimumNumberOfOperationsToMoveAllBallsToEachBox.java
+++ code/java/main/leet_code/medium/MinimumNumberOfOperationsToMoveAllBallsToEachBox.java
@@ -2,14 +2,13 @@ package leet_code.medium;
 
 public class MinimumNumberOfOperationsToMoveAllBallsToEachBox {
     public int[] minOperations(String boxes) {
-        int[] left = new int[boxes.length()];
-        int[] right = new int[boxes.length()];
         int[] res = new int[boxes.length()];
-
         int count = 0;
+        int sum = 0;
+
         for (int i = 0; i < boxes.length(); i++) {
             if (i > 0) {
-                left[i] = left[i - 1] + count;
+                res[i] = res[i - 1] + count;
             }
             if (boxes.charAt(i) == '1') {
                 count++;
@@ -19,17 +18,13 @@ public class MinimumNumberOfOperationsToMoveAllBallsToEachBox {
         count = 0;
         for (int i = boxes.length() - 1; i >= 0; i--) {
             if (i < boxes.length() - 1) {
-                right[i] = right[i + 1] + count;
+                sum += count;
+                res[i] += sum;
             }
             if (boxes.charAt(i) == '1') {
                 count++;
             }
         }
-
-        for (int i = 0; i < boxes.length(); i++) {
-            res[i] = left[i] + right[i];
-        }
-
         return res;
     }
 }
