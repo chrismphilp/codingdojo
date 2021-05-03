c2527ea2b3529882d8119035e93ce81346be5de3
Christopher Philp <chrismphilp@hotmail.co.uk>
2021-05-03 12:06:36 +0100

    [master] | Adding solution for SelfDividingNumbers

---
+++ code/java/main/leet_code/easy/RunningSumOf1dArray.java
@@ -0,0 +1,11 @@
+package leet_code.easy;
+
+public class RunningSumOf1dArray {
+    public int[] runningSum(int[] nums) {
+        if (nums.length <= 1) return nums;
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+}
