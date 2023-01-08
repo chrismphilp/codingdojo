fb9cc4c8c7758933f824d7d30b09292228f1ed96
Christopher Philp <chrismphilp@hotmail.co.uk>
2023-01-08 13:06:09 +0000

    [master] | Adding initial AddTwoNumbers solution

--- code/cpp/src/leetcode/easy/add-two-numbers/AddTwoNumbers.h
+++ code/cpp/src/leetcode/easy/add-two-numbers/AddTwoNumbers.h
@@ -4,8 +4,6 @@ struct ListNode {
     int val;
     ListNode* next;
     ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 class AddTwoNumbers {
